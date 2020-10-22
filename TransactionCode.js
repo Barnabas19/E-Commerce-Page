ee0d8cd9bf6bbf4061dbd339f2ff26f6a4397466
Barnabas Babatunde <64215035+Barnabas19@users.noreply.github.com>
2020-10-22 20:31:59 +0100

    new changes

--- TransactionCode.js
+++ TransactionCode.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //list of store item prices by array destructuring
 const[tumeric, kfcBread, wrathOfGodPepper, waffle, milk, soda, pasta,
     marshmallow, cashew, mayonnaise, cornFlakes, bottleWater, pork,
@@ -6,21 +8,36 @@ const[tumeric, kfcBread, wrathOfGodPepper, waffle, milk, soda, pasta,
         100, 900, 200, 150, 700, 50, 300, 170, 200, 120, 500, 100,
         250, 500];
 
+//focusing on first item after page first loads (for accessibility)
+const firstButton = document.getElementById("first-button");
+firstButton.focus();
 
-let itemsToBePurchased = [];    //data structure containing items to be purchased
-let totalCost = 0;      //initial total cost of items to be purchased
+
+let itemsToBePurchased = []; //data structure containing items to be purchased
+let totalCost = 0; //initial total cost of items to be purchased
 let debit = 0;
 
 
 //adding items to be purchased to data structure
+const promptSection = document.querySelector(".prompt");
+const enterButton = document.querySelector(".enter");
+const inputPrompt = document.getElementById("number-of-units");
+let item;
 const button = document.querySelectorAll(".button");
 button.forEach(function(eachButton){
     eachButton.addEventListener("click", function(){
-        const item = String(eachButton.textContent);
-        const numberOfUnits = Number(window.prompt("Please enter the number of units"));
-        for(let i=0; i<numberOfUnits; i++){
-            itemsToBePurchased.push(item);
-        }
+        item = String(eachButton.textContent);
+        promptSection.style.display = "block";
+        inputPrompt.focus();
+        enterButton.addEventListener("click", function(){
+            const noOfUnits = Number(inputPrompt.value);
+            for(let i=0; i<noOfUnits; i++){
+                itemsToBePurchased.push(item);
+            }
+            promptSection.style.display = "none";
+            inputPrompt.value = null;
+            eachButton.focus();
+        });
     });
 });
 
@@ -30,6 +47,10 @@ OKButton.addEventListener("click", function(){
     updateTotalCost(itemsToBePurchased);
     zeroCostAndItems();
 });
+
+//for the bill notice section
+const billNotice = document.querySelector(".bill-notice");
+const purchaseDetails = document.querySelector(".purchase-details");
 
 
 function updateTotalCost(items){  //Updates total cost
@@ -118,9 +139,14 @@ function updateTotalCost(items){  //Updates total cost
             default:
                 continue;
         }
-    }
-    window.alert(`Your bill is ${String(totalCost)} Naira. Please enter your account details to complete transaction`);
+    } 
+    billNotice.style.display = "block";
+    purchaseDetails.textContent = `Your bill is ${String(totalCost)} Naira. Please enter your account details to complete transaction`;
 }
+const proceed = document.querySelector(".proceed");
+proceed.addEventListener("click", function(){
+    billNotice.style.display = "none";
+});
 
 function zeroCostAndItems(){  //zeroes total cost and items to be purchased  
     debit = totalCost;
@@ -132,18 +158,21 @@ function zeroCostAndItems(){  //zeroes total cost and items to be purchased
 //Transaction Completion Form
 let accountName;
 let accountNumber;
+const input = document.querySelectorAll(".input");
 const submit = document.querySelector(".submit-button");
+const invalid = document.querySelector(".invalid");
+const invalidParagraph = document.querySelector(".invalid-paragraph");
 submit.addEventListener("click", function(){
-    const input = document.querySelectorAll(".input");
     for(let i=0; i<input.length; i++){
         if(isNaN(parseInt(input[i].value))){  //checks if input is user's account name or account number
-            accountName = String(input[i].value);     //input is user's account name
-        }else{                                //input is user's account number
+            accountName = String(input[i].value);  //input is user's account name
+        }else{  //input is user's account number
             if(input[i].value.length == 10){
                 accountNumber = input[i].value;
             }
             else{
-                window.alert(`Invalid Account Number. Please re-select items and enter a correct account number`);
+                invalid.style.display = "block";
+                invalidParagraph.textContent = "INVALID ACCOUNT NUMBER";
             }
         }
     }
@@ -151,14 +180,24 @@ submit.addEventListener("click", function(){
 });
 
 
-//REPLACEMENT FOR BANKING API
+//REPLACEMENT FOR BANKING API;
 function checkAffordability(){
+    const successful = document.querySelector(".successful");
+    const successfulParagraph = document.querySelector(".successful-paragraph");
     if(accountNumber){
         if(accountName.length >= 28){
-            window.alert(`Transaction successful! You have been debited by an amount of ${debit} Naira`);
+            successful.style.display = "block";
+            successfulParagraph.textContent = `Transaction successful! You have been debited by an amount of ${debit} Naira`;
         }else{
-            window.alert(`You do not have sufficient funds`);
+            successful.style.display = "block";
+            successfulParagraph.textContent = `Sorry, you do not have sufficient funds to proceed with this transaction.`;
         }
     }
 }
+
+/*
+const end = document.querySelector(".end");
+end.addEventListener("click", function(){
+    successful.style.display = "none";
+});*/
 
